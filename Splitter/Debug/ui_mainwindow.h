9d724cb7d18d3ee6377c1bfce1dd8205981cf22d
Mars_xiaolei <18328035868@163.com>
2019-08-01 17:52:22 +0800

    Qt窗口使用分裂器进行布局
    纯设计师，仿照Qt的界面设计了一个简单的示例，窗口可以改变大小

---
+++ Splitter/Debug/ui_mainwindow.h
@@ -0,0 +1,228 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.6
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QSplitter>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QTreeWidget>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QGridLayout *gridLayout;
+    QSplitter *splitter_2;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QTreeWidget *treeWidget;
+    QSplitter *splitter;
+    QWidget *widget1;
+    QVBoxLayout *verticalLayout_2;
+    QLabel *label_2;
+    QTextEdit *textEdit;
+    QWidget *widget2;
+    QVBoxLayout *verticalLayout_3;
+    QLabel *label_3;
+    QTextEdit *textEdit_2;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(1024, 768);
+        MainWindow->setMinimumSize(QSize(1024, 768));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        gridLayout = new QGridLayout(centralWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        splitter_2 = new QSplitter(centralWidget);
+        splitter_2->setObjectName(QStringLiteral("splitter_2"));
+        splitter_2->setOrientation(Qt::Horizontal);
+        widget = new QWidget(splitter_2);
+        widget->setObjectName(QStringLiteral("widget"));
+        verticalLayout = new QVBoxLayout(widget);
+        verticalLayout->setSpacing(0);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(widget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);\n"
+"background-color: rgb(186, 151, 200);\n"
+"font: 9pt \"\345\276\256\350\275\257\351\233\205\351\273\221\";"));
+
+        verticalLayout->addWidget(label);
+
+        treeWidget = new QTreeWidget(widget);
+        QTreeWidgetItem *__qtreewidgetitem = new QTreeWidgetItem(treeWidget);
+        new QTreeWidgetItem(__qtreewidgetitem);
+        new QTreeWidgetItem(__qtreewidgetitem);
+        QTreeWidgetItem *__qtreewidgetitem1 = new QTreeWidgetItem(treeWidget);
+        new QTreeWidgetItem(__qtreewidgetitem1);
+        new QTreeWidgetItem(__qtreewidgetitem1);
+        new QTreeWidgetItem(__qtreewidgetitem1);
+        QTreeWidgetItem *__qtreewidgetitem2 = new QTreeWidgetItem(treeWidget);
+        new QTreeWidgetItem(__qtreewidgetitem2);
+        new QTreeWidgetItem(__qtreewidgetitem2);
+        QTreeWidgetItem *__qtreewidgetitem3 = new QTreeWidgetItem(treeWidget);
+        new QTreeWidgetItem(__qtreewidgetitem3);
+        treeWidget->setObjectName(QStringLiteral("treeWidget"));
+
+        verticalLayout->addWidget(treeWidget);
+
+        splitter_2->addWidget(widget);
+        splitter = new QSplitter(splitter_2);
+        splitter->setObjectName(QStringLiteral("splitter"));
+        splitter->setOrientation(Qt::Vertical);
+        widget1 = new QWidget(splitter);
+        widget1->setObjectName(QStringLiteral("widget1"));
+        verticalLayout_2 = new QVBoxLayout(widget1);
+        verticalLayout_2->setSpacing(0);
+        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        label_2 = new QLabel(widget1);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);\n"
+"background-color: rgb(186, 151, 200);\n"
+"font: 9pt \"\345\276\256\350\275\257\351\233\205\351\273\221\";"));
+
+        verticalLayout_2->addWidget(label_2);
+
+        textEdit = new QTextEdit(widget1);
+        textEdit->setObjectName(QStringLiteral("textEdit"));
+
+        verticalLayout_2->addWidget(textEdit);
+
+        splitter->addWidget(widget1);
+        widget2 = new QWidget(splitter);
+        widget2->setObjectName(QStringLiteral("widget2"));
+        verticalLayout_3 = new QVBoxLayout(widget2);
+        verticalLayout_3->setSpacing(0);
+        verticalLayout_3->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
+        label_3 = new QLabel(widget2);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);\n"
+"background-color: rgb(186, 151, 200);\n"
+"font: 9pt \"\345\276\256\350\275\257\351\233\205\351\273\221\";"));
+
+        verticalLayout_3->addWidget(label_3);
+
+        textEdit_2 = new QTextEdit(widget2);
+        textEdit_2->setObjectName(QStringLiteral("textEdit_2"));
+
+        verticalLayout_3->addWidget(textEdit_2);
+
+        splitter->addWidget(widget2);
+        splitter_2->addWidget(splitter);
+
+        gridLayout->addWidget(splitter_2, 0, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralWidget);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "\347\252\227\345\217\243\345\210\206\345\211\262\345\231\250", Q_NULLPTR));
+        label->setText(QApplication::translate("MainWindow", "\351\241\271\347\233\256", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem = treeWidget->headerItem();
+        ___qtreewidgetitem->setText(0, QApplication::translate("MainWindow", "Project", Q_NULLPTR));
+
+        const bool __sortingEnabled = treeWidget->isSortingEnabled();
+        treeWidget->setSortingEnabled(false);
+        QTreeWidgetItem *___qtreewidgetitem1 = treeWidget->topLevelItem(0);
+        ___qtreewidgetitem1->setText(0, QApplication::translate("MainWindow", "\345\244\264\346\226\207\344\273\266", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem2 = ___qtreewidgetitem1->child(0);
+        ___qtreewidgetitem2->setText(0, QApplication::translate("MainWindow", "1.h", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem3 = ___qtreewidgetitem1->child(1);
+        ___qtreewidgetitem3->setText(0, QApplication::translate("MainWindow", "2.h", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem4 = treeWidget->topLevelItem(1);
+        ___qtreewidgetitem4->setText(0, QApplication::translate("MainWindow", "\346\272\220\346\226\207\344\273\266", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem5 = ___qtreewidgetitem4->child(0);
+        ___qtreewidgetitem5->setText(0, QApplication::translate("MainWindow", "main.cpp", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem6 = ___qtreewidgetitem4->child(1);
+        ___qtreewidgetitem6->setText(0, QApplication::translate("MainWindow", "1.cpp", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem7 = ___qtreewidgetitem4->child(2);
+        ___qtreewidgetitem7->setText(0, QApplication::translate("MainWindow", "2.cpp", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem8 = treeWidget->topLevelItem(2);
+        ___qtreewidgetitem8->setText(0, QApplication::translate("MainWindow", "\350\265\204\346\272\220\346\226\207\344\273\266", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem9 = ___qtreewidgetitem8->child(0);
+        ___qtreewidgetitem9->setText(0, QApplication::translate("MainWindow", "1.ui", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem10 = ___qtreewidgetitem8->child(1);
+        ___qtreewidgetitem10->setText(0, QApplication::translate("MainWindow", "2.ui", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem11 = treeWidget->topLevelItem(3);
+        ___qtreewidgetitem11->setText(0, QApplication::translate("MainWindow", "\345\205\266\344\273\226\346\226\207\344\273\266", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem12 = ___qtreewidgetitem11->child(0);
+        ___qtreewidgetitem12->setText(0, QApplication::translate("MainWindow", "other", Q_NULLPTR));
+        treeWidget->setSortingEnabled(__sortingEnabled);
+
+        label_2->setText(QApplication::translate("MainWindow", "\347\274\226\350\276\221\344\273\243\347\240\201", Q_NULLPTR));
+        textEdit->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'SimSun'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">#include &lt;iostream&gt;</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">using namespace std;</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; m"
+                        "argin-right:0px; -qt-block-indent:0; text-indent:0px;\">int main()</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">{</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">  cout&lt;&lt;&quot;Hello World !&quot;&lt;&lt;endl;</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">  return 0;</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">}</p></body></html>", Q_NULLPTR));
+        label_3->setText(QApplication::translate("MainWindow", "\345\272\224\347\224\250\347\250\213\345\272\217\350\276\223\345\207\272", Q_NULLPTR));
+        textEdit_2->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'SimSun'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">\350\276\223\345\207\272\347\273\223\346\236\234\357\274\232Hello World !</p></body></html>", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
